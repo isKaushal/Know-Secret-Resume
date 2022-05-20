da113d20830714515ae99f464b2e31efccbf1bd7
kaushal kumar soni <kaushalkumarsoni69@gmail.com>
2022-05-20 18:50:24 +0530

    Resume

---
+++ js/navigation.js
@@ -0,0 +1,22 @@
+
+let Home = document.getElementById("home");
+let User = document.getElementById("user");
+let NavBar = document.getElementById("navigation");
+let CenterTile = document.getElementById("main-tile2");
+
+// Home.addEventListener("click",()=>{
+
+    // NavBar.style.width = "20rem"
+    // CenterTile.style.width = "62rem"
+    
+// })
+
+// User.addEventListener("click",()=>{
+
+    NavBar.style.width = "5.5rem"
+    CenterTile.style.width = "79.5rem"
+
+    
+// })
+
+
